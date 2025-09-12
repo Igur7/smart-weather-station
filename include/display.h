40bf7b6e5e120100698423eb51c9883e82efb7b7
Igur7 <115376029+Igur7@users.noreply.github.com>
2025-09-12 15:51:35 +0200

    Update display.h

--- include/display.h
+++ include/display.h
@@ -2,19 +2,20 @@
 #define DISPLAY_H
 
 #include <Arduino.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_ST7789.h>
 #include "json_parser.h"
 
-#define TFT_CS    5
-#define TFT_DC    2
-#define TFT_RST   4
-#define TFT_BL    15
-
-extern Adafruit_ST7789 tft;
+#define TFT_CS   5
+#define TFT_DC   2
+#define TFT_RST  4
+#define TFT_BL   15
 
 void initDisplay();
 void showCurrentWeather(const WeatherInfo& info);
-void showForecast(const String& json);
+void showDayForecast(const ForecastDay& day, size_t dayIndex);
+
+void drawSunIcon(int x, int y, uint16_t color);
+void drawCloudIcon(int x, int y, uint16_t color);
+void drawRainIcon(int x, int y, uint16_t color);
+void drawSnowIcon(int x, int y, uint16_t color);
 
 #endif
