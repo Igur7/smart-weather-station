86faa8fb7ebb9ab86b9d5792a51b43d2bca67c51
Igur7 <115376029+Igur7@users.noreply.github.com>
2025-09-27 13:53:51 +0200

    Update display.h

--- include/display.h
+++ include/display.h
@@ -10,12 +10,16 @@
 #define TFT_BL   15
 
 void initDisplay();
-void showCurrentWeather(const WeatherInfo& info);
-void showDayForecast(const ForecastDay& day, size_t dayIndex);
-
+void showCurrentWeather(const WeatherInfo& info, float sensorTemp);
+void showForecastRow(const ForecastDay& day);
 void drawSunIcon(int x, int y, uint16_t color);
 void drawCloudIcon(int x, int y, uint16_t color);
 void drawRainIcon(int x, int y, uint16_t color);
 void drawSnowIcon(int x, int y, uint16_t color);
+void drawDropIcon(int x, int y, uint16_t color);
+void drawSunWithCloudIcon(int x, int y, uint16_t color);
+void drawSunriseIcon(int x, int y, uint16_t color);
+void drawSunsetIcon(int x, int y, uint16_t color);
+void drawThermometerIcon(int x, int y, uint16_t color);
 
 #endif
