15668523e23512007ef5e5fbc763d7e4e436db88
Igur7 <115376029+Igur7@users.noreply.github.com>
2025-11-02 22:19:21 +0100

    upgrade
    Refactored WiFi connection logic to include mDNS setup and hostname configuration.

--- src/wifi_setup.cpp
+++ src/wifi_setup.cpp
@@ -1,17 +1,20 @@
 #include <WiFi.h>
-#include "wifi_setup.h"
+#include <ESPmDNS.h>
 
 void connectToWiFi(const char* ssid, const char* password) {
-    WiFi.begin(ssid, password);
-    Serial.print("Łączenie z WiFi ");
-    Serial.print(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.setHostname("pogoda-device");  // Ustaw nazwę hosta WiFi
+  WiFi.begin(ssid, password);
+  Serial.print("Łączenie z WiFi: ");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nPołączono. IP: " + WiFi.localIP().toString());
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-
-    Serial.println("\nPołączono z WiFi!");
-    Serial.print("Adres IP: ");
-    Serial.println(WiFi.localIP());
+  if (!MDNS.begin("pogoda")) {               // <-- nazwa: pogoda.local
+    Serial.println("błąd mDNS");
+  } else {
+    Serial.println("mDNS uruchomione: http://pogoda.local");
+  }
 }
